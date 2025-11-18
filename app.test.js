9aa162a28c46f5edf9c99f0d8c3a64164ba5ed53
alokkumar07 <aloknith07@gmail.com>
2025-11-18 22:33:21 +0530

    some more test case in jest

--- app.test.js
+++ app.test.js
@@ -4,3 +4,18 @@ test('sortingByAge sorts users by age in ascending order', () => {
     const sortedUsers = sortingByAge();
     expect(sortedUsers[0].name).toBe("Alok");
 });
+
+test('test for last member', () => {      
+    const sortedUsers = sortingByAge();
+    expect(sortedUsers[sortedUsers.length-1].name).toBe("Charlie");
+});
+
+test('test for total sorted length', () => {      
+    const sortedUsers = sortingByAge();
+    expect(sortedUsers.length).toBe(4);
+});
+
+test('test for data undefined', () => {      
+    const sortedUsers = sortingByAge();
+    expect(sortedUsers).not.toBe(undefined);
+});
